fb46eac2d0cc766f6f587b0af7dcfb4cafec4abe
Krijesh Maharjan <krijeshmaharjan16@gmail.com>
2026-02-26 08:57:00 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__== "__main__":
+    app.run(debug=True,port=4848)
+
